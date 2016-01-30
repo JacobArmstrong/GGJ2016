b6f5870ead894f430b497c02d3cd1c29bbcb7107
Z <patrickmcnealy@gmail.com>
2016-01-30 01:09:01 -0500

    Stuff?

---
+++ Dungeon Spells/The Ritual/Assets/CombatInputHandling.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatInputHandling : MonoBehaviour {
+	private float hInput, vInput;
+	
+	private GameObject upArrow, downArrow, leftArrow, rightArrow;
+	public GameObject[] arrows = new GameObject[4];
+
+    public GameObject[] slots = new GameObject[8];
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
