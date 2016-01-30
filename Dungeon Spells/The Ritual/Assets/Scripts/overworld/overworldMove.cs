069b9dd6978572048cae3038a15a14af8201a75b
tanderson0 <thomasastuff@gmail.com>
2016-01-30 18:04:33 -0500

    ayy

--- Dungeon Spells/The Ritual/Assets/Scripts/overworld/overworldMove.cs
+++ Dungeon Spells/The Ritual/Assets/Scripts/overworld/overworldMove.cs
@@ -1,26 +1,76 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class overworldMove : MonoBehaviour {
+public class overworldMove : MonoBehaviour
+{
 
     public bool isTurn = true;
     public float moveScale = 1.0f;
     public float waitTime = 0.75f;
-    private float timer = 0.0f;
+    private int index;
 
+    public float timeBetweenSprites;
+    private float timer;
+
+    private SpriteRenderer renderer;
+
+    public Sprite[] up = new Sprite[0];
+    public Sprite[] left = new Sprite[0];
+    public Sprite[] down = new Sprite[0];
+    public Sprite[] right = new Sprite[0];
+
+    private Transform body;
     // Use this for initialization
     void Start ()
     {
-        
+        Transform body = GetComponent<Transform>();
+        timer = timeBetweenSprites;
+        renderer.GetComponent<SpriteRenderer>();
+        renderer.sprite = down[0];
     }
 	
 	// Update is called once per frame
-	void Update ()
+	
+    void Update()
     {
-        timer += 1.0f * Time.deltaTime;
+        timer -= Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    /*void Update ()
+    {
+        timer += Time.deltaTime;
         if(isTurn)
         {
-            BoxCollider2D body = GetComponent<BoxCollider2D>();
             if (timer > waitTime)
             {
                 if (Input.GetKeyDown(KeyCode.W))
@@ -54,5 +104,5 @@ public class overworldMove : MonoBehaviour {
             }
             }
         }
-	}
+	}*/
 }
