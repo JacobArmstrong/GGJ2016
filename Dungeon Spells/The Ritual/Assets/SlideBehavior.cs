2b44871dfea8024035e78ae43ceb8a31f314ca9b
JacobArmstrong <jarmstrong1695@gmail.com>
2016-01-30 07:50:14 -0500

    getting latest and greatest stuff

--- Dungeon Spells/The Ritual/Assets/SlideBehavior.cs
+++ Dungeon Spells/The Ritual/Assets/SlideBehavior.cs
@@ -3,15 +3,20 @@ using System.Collections;
 
 public class SlideBehavior : MonoBehaviour {
 	public Vector2 targetLocation;
-	public float slideSpeed;
+	public float slideLerpRate;
+
+	private Vector2 startPosition;
+	private float slideDistance;
 	// Use this for initialization
 	void Start () {
+		startPosition = transform.position;
+		slideDistance = Vector2.Distance (startPosition, targetLocation);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//I'm actually using this function the wrong way, but I like the way it looks
-		transform.position = Vector2.Lerp(transform.position, targetLocation, slideSpeed*Time.deltaTime);
+		transform.position = Vector2.Lerp (transform.position, targetLocation, slideLerpRate * Time.deltaTime);
 	}
 
 	void OnDrawGizmosSelected() {
