df435a6e0fa2e74522c32e83bafb6e8524701451
Z <patrickmcnealy@gmail.com>
2016-01-30 07:06:52 -0500

    I got stuff happening here!

---
+++ Dungeon Spells/The Ritual/Assets/Scripts/Combat/CombatSpellbook.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatSpellbook : MonoBehaviour {
+
+
+    public GameObject SpellAnimation;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GetComponent<CombatInputHandling>().isReady())
+        {
+            int[] spell = GetComponent<CombatInputHandling>().TakeSpell();
+            string spellName = IdentifySpell(spell);
+            if(spellName != "")
+            {
+                SpellAnimation.GetComponent<SpellCaster>().setSpell(spellName);
+            }
+        }
+    }
+
+    private string IdentifySpell(int[] spell)
+    {
+        string spellName = "";
+        if(spell[0] == 0)
+        {
+            if(spell[1] == 0)
+            {
+                if(spell[2] == 0)
+                {
+                    if(spell[3] == 0)
+                    {
+                        if(spell[4] == 0)
+                        {
+                            spellName = "fire";
+                        }
+                    }
+                }
+            }
+        }
+        return spellName;
+
+    }
+}
---
+++ Dungeon Spells/The Ritual/Assets/Scripts/Combat/SpellCaster.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpellCaster : MonoBehaviour {
+
+    private int timer = -1;
+    public Sprite fireSpell;
+    public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(timer > 0)
+        {
+            timer--;
+        }
+        else if(timer == 0)
+        {
+            timer--;
+            GetComponent<SpriteRenderer>().sprite = null;
+        }
+	}
+
+    public void setSpell(string spellName)
+    {
+        timer = 50;
+        int enemyDamage = 0;
+        if(spellName == "fire")
+        {
+            GetComponent<SpriteRenderer>().sprite = fireSpell;
+            enemyDamage = 10;
+        }
+
+        enemy.GetComponent<EntityStats>().Damage(enemyDamage);
+
+    }
+}
