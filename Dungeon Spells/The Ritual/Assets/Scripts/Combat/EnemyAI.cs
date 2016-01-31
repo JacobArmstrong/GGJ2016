41ef87531450ce1b2dcbcffd63a180d511dbd4ff
Z <patrickmcnealy@gmail.com>
2016-01-31 12:40:13 -0500

    combat stuffs

--- Dungeon Spells/The Ritual/Assets/Scripts/Combat/EnemyAI.cs
+++ Dungeon Spells/The Ritual/Assets/Scripts/Combat/EnemyAI.cs
@@ -5,6 +5,11 @@ public class EnemyAI : MonoBehaviour {
 
     public GameObject Caster;
     int timer = 300;
+    int animTimer = -1;
+
+    public GameObject Player;
+
+    public Sprite[] sprites = new Sprite[2];
 	// Use this for initialization
 	void Start () {
 	
@@ -17,6 +22,18 @@ public class EnemyAI : MonoBehaviour {
         {
             Caster.GetComponent<SpellCaster>().setSpell("Evisceration");
             timer = 300;
+            Player.GetComponent<SpriteRenderer>().sprite = sprites[1];
+            animTimer = 50;
         }
-	}
+
+        if (animTimer > 0)
+        {
+            animTimer--;
+        }
+        else if (animTimer == 0)
+        {
+            animTimer--;
+            Player.GetComponent<SpriteRenderer>().sprite = sprites[0];
+        }
+    }
 }
