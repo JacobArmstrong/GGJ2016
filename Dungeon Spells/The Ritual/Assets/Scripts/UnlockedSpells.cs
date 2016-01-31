610d95d28524e9330f323dc0c162cd8e858b3151
JacobArmstrong <jarmstrong1695@gmail.com>
2016-01-31 10:56:27 -0500

    switching bewteen combat

--- Dungeon Spells/The Ritual/Assets/Scripts/UnlockedSpells.cs
+++ Dungeon Spells/The Ritual/Assets/Scripts/UnlockedSpells.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class UnlockedSpells : MonoBehaviour {
 
@@ -15,6 +16,14 @@ public class UnlockedSpells : MonoBehaviour {
     // 8 = arcane (mod)
 
     private static bool[] SpellStatus = { true, true, true, true, false, false, false, false, false };
+
+    //coordinates for the player's last known overworld position
+    public static Vector3 lastKnownPosition;
+    public static bool bWasInCombat = false;
+    public static int lastLevelIndex;
+
+    public static List<int> defeatedEnemies = new List<int>();
+
 
 	// Use this for initialization
 	void Start () {
