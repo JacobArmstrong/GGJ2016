476fcade5d8a2cc93529a353019a681300fda849
JacobArmstrong <jarmstrong1695@gmail.com>
2016-01-30 21:29:04 -0500

    working movement

--- Dungeon Spells/The Ritual/Assets/Scripts/Combat/CombatInputHandling.cs
+++ Dungeon Spells/The Ritual/Assets/Scripts/Combat/CombatInputHandling.cs
@@ -27,7 +27,7 @@ public class CombatInputHandling : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (spellbook.GetComponent<SpellBook>().getBGetOnScreen() == false)
+        if (spellbook.GetComponent<KnownSpells>().GetBOnScreen() == false)
         {
             if (Input.GetKeyDown(KeyCode.W))
             {
--- Dungeon Spells/The Ritual/Assets/Scripts/overworld/overworldMove.cs
+++ Dungeon Spells/The Ritual/Assets/Scripts/overworld/overworldMove.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using UnityEngine;
 using System.Collections;
 
@@ -8,10 +7,11 @@ public class overworldMove : MonoBehaviour
     public float moveSpeed;
 
     private new SpriteRenderer renderer;
-    private Vector3 targetPosition;
+    public Vector3 targetPosition;
     private Vector3 beginPosition;
     private float timeSpentMoving;
     private bool bIsMoving;
+    private float journeyLength;
 
     //Arrays to hold the sprites
     public Sprite[] up = new Sprite[0];
@@ -31,38 +31,45 @@ public class overworldMove : MonoBehaviour
     {
         //checking the possible moves
         if (!bIsMoving) {
-            //check up
-            //first, do a raycast in the direction we want to go clamped by the distance we want to go
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.up, moveAmount);
-            //if the raycast doesn't hit anything, the move is possible to do
-            if (hit.collider == null)
+            if (Input.GetKey(KeyCode.W))
             {
-                targetPosition = Vector3.up * moveAmount;
-                initMovement();
+                //check up
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.up, moveAmount);
+                if (hit.collider == null)
+                {
+                    targetPosition += Vector3.up * moveAmount;
+                    initMovement();
+                }
             }
-            //check down
-            hit = Physics2D.Raycast(transform.position, Vector3.down, moveAmount);
-            //if the raycast doesn't hit anything, the move is possible to do
-            if (hit.collider == null)
+            else if (Input.GetKey(KeyCode.S))
             {
-                targetPosition = Vector3.down * moveAmount;
-                initMovement();
+                //check down
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.down, moveAmount);
+                if (hit.collider == null)
+                {
+                    targetPosition += Vector3.down * moveAmount;
+                    initMovement();
+                }
             }
-            //check left
-            hit = Physics2D.Raycast(transform.position, Vector3.left, moveAmount);
-            //if the raycast doesn't hit anything, the move is possible to do
-            if (hit.collider == null)
+            else if (Input.GetKey(KeyCode.A))
             {
-                targetPosition = Vector3.left * moveAmount;
-                initMovement();
+                //check left
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.left, moveAmount);
+                if (hit.collider == null)
+                {
+                    targetPosition += Vector3.left * moveAmount;
+                    initMovement();
+                }
             }
-            //check right
-            hit = Physics2D.Raycast(transform.position, Vector3.right, moveAmount);
-            //if the raycast doesn't hit anything, the move is possible to do
-            if (hit.collider == null)
+            else if (Input.GetKey(KeyCode.D))
             {
-                targetPosition = Vector3.right * moveAmount;
-                initMovement();
+                //check right
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.right, moveAmount);
+                if (hit.collider == null)
+                {
+                    targetPosition += Vector3.right * moveAmount;
+                    initMovement();
+                }
             }
         }
 
@@ -70,6 +77,18 @@ public class overworldMove : MonoBehaviour
         if (bIsMoving)
         {
             timeSpentMoving += Time.deltaTime;
+            if(transform.position != targetPosition)
+            {
+                float distCovered = timeSpentMoving / moveSpeed;
+                float fracJourney = distCovered / moveAmount;
+                transform.position = Vector3.Lerp(beginPosition, targetPosition, fracJourney);
+            }
+            else
+            {
+                bIsMoving = false;
+                timeSpentMoving = 0.0f;
+                targetPosition = transform.position;
+            }
         }
     }
 
@@ -77,115 +96,6 @@ public class overworldMove : MonoBehaviour
     {
         bIsMoving = true;
         beginPosition = transform.position;
+        journeyLength = Vector3.Distance(transform.position, targetPosition);
     }
 }
-=======
-﻿using UnityEngine;
-using System.Collections;
-
-public class overworldMove : MonoBehaviour
-{
-
-    public bool isTurn = true;
-    public float moveScale = 1.0f;
-    public float waitTime = 0.75f;
-    private int index;
-
-    public float timeBetweenSprites;
-    private float timer;
-
-    private SpriteRenderer renderer;
-
-    public Sprite[] up = new Sprite[0];
-    public Sprite[] left = new Sprite[0];
-    public Sprite[] down = new Sprite[0];
-    public Sprite[] right = new Sprite[0];
-
-    private Transform body;
-    // Use this for initialization
-    void Start ()
-    {
-        Transform body = GetComponent<Transform>();
-        timer = timeBetweenSprites;
-        renderer.GetComponent<SpriteRenderer>();
-        renderer.sprite = down[0];
-    }
-	
-	// Update is called once per frame
-	
-    void Update()
-    {
-        timer -= Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            
-        }
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-
-        }
-
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-
-        }
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-    /*void Update ()
-    {
-        timer += Time.deltaTime;
-        if(isTurn)
-        {
-            if (timer > waitTime)
-            {
-                if (Input.GetKeyDown(KeyCode.W))
-                {
-                    body.transform.position += Vector3.up * moveScale;
-                    timer = 0.0f;
-                }
-            if (timer > waitTime)
-            {
-                if (Input.GetKeyDown(KeyCode.A))
-                {
-                    body.transform.position += Vector3.left * moveScale;
-                    timer = 0.0f;
-                }
-            }
-            if (timer > waitTime)
-            {
-                if (Input.GetKeyDown(KeyCode.S))
-                {
-                    body.transform.position += Vector3.down * moveScale;
-                    timer = 0.0f;
-                }
-            }
-            if (timer > waitTime)
-            {
-                if (Input.GetKeyDown(KeyCode.D))
-                {
-                    body.transform.position += Vector3.right * moveScale;
-                    timer = 0.0f;
-                }
-            }
-            }
-        }
-	}*/
-}
->>>>>>> 0a70c7e092b58c0099094d60eb2b279be43ed81a
